1fee7baa72f0b60c46f9e9a9ff1d296279859d4d
DanishOnGit <youcandoit1911@gmail.com>
2021-03-15 23:13:11 +0530

    avatars,badges added to docs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const myModal=document.querySelector(".modal-wrapper");
 const closeBtn=document.querySelector("#closeMe");
-const closeBtn2=document.querySelector("#closeIt");
+const closeBtn2=document.querySelector(".fa-times");
+
 
 closeBtn.addEventListener("click",()=>myModal.style.display="none");
-closeBtn2.addEventListener("click",()=>myModal.style.display="none");
+closeBtn2.addEventListener("click",(e)=>e.target.style.display="none");
 
 window.onclick =(event)=>{
     console.log(event.target)
