2dfc5e9482bb5cf0889ed22b02493c903d88020f
DanishOnGit <youcandoit1911@gmail.com>
2021-03-16 15:46:51 +0530

    added modals,inputs,navbars

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const myModal=document.querySelector(".modal-wrapper");
 const closeBtn=document.querySelector("#closeMe");
-const closeBtn2=document.querySelector(".fa-times");
+const closeBtn2=document.querySelector("#closeIt");
+const modalLaunch=document.getElementById("launchModal")
 
-
+modalLaunch.addEventListener("click",()=>myModal.style.display="block");
 closeBtn.addEventListener("click",()=>myModal.style.display="none");
-closeBtn2.addEventListener("click",(e)=>e.target.style.display="none");
+closeBtn2.addEventListener("click",()=>myModal.style.display="none");
 
-window.onclick =(event)=>{
-    console.log(event.target)
-    myModal.style.display = "none";
-    // if (event.target !== myModal) {
-    //   myModal.style.display = "none";
-    // }
-  }
+// window.onclick =(event)=>{
+//     console.log(event.target)
+//     myModal.style.display = "none";
+//     // if (event.target !== myModal) {
+//     //   myModal.style.display = "none";
+//     // }
+//   }
