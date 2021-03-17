4d656edd252275feb075a207042e092578d42860
DanishOnGit <youcandoit1911@gmail.com>
2021-03-18 01:02:49 +0530

    new card and css file added

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const closeBtn=document.querySelector("#closeMe");
 const closeBtn2=document.querySelector("#closeIt");
 const modalLaunch=document.getElementById("launchModal")
 
+// window.onclick =(event)=>{
+//     if (event.target !== myModal) {
+//       myModal.style.display = "none";
+//     }
+//   }
+
 modalLaunch.addEventListener("click",()=>myModal.style.display="block");
 closeBtn.addEventListener("click",()=>myModal.style.display="none");
 closeBtn2.addEventListener("click",()=>myModal.style.display="none");
 
-// window.onclick =(event)=>{
-//     console.log(event.target)
-//     myModal.style.display = "none";
-//     // if (event.target !== myModal) {
-//     //   myModal.style.display = "none";
-//     // }
-//   }
+function myFunction() {
+    const copyText = document.getElementById("cssUrl");
+    const popUpElement=document.getElementById("myPopUp");
+    navigator.clipboard.writeText("https://popcube-ui.netlify.app/main.css");
+    popUpElement.classList.toggle("showPopUp")
+
+  }
+ 
+
